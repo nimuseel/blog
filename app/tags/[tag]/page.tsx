e1893b64339b7a330010faf42e185aa0e28144c9
nimuseel <l08c0739@gmail.com>
2025-03-02 14:45:17 +0900

    chore: prettier 패키지 업데이트 및 lint 룰 개선

--- app/tags/[tag]/page.tsx
+++ app/tags/[tag]/page.tsx
@@ -1,31 +1,31 @@
-import { PostCard } from 'nextra-theme-blog'
-import { getPosts, getTags } from '../../posts/get-posts'
+import { PostCard } from 'nextra-theme-blog';
+import { getPosts } from '@/app/posts/get-posts';
 
 interface Params {
-  tag: string
+  tag: string;
 }
 
 async function generateMetadata(props: Params) {
   return {
-    title: `${decodeURIComponent(props.tag)}`
-  }
+    title: `${decodeURIComponent(props.tag)}`,
+  };
 }
 
 export default async function TagPage(props) {
-  const params: { tag: string } = await props.params
-  const { title } = await generateMetadata(params)
-  console.log("🚀 ~ TagPage ~ title:", title)
-  const posts = await getPosts()
+  const params: { tag: string } = await props.params;
+  const { title } = await generateMetadata(params);
+  const posts = await getPosts();
+
   return (
     <>
       <h1>{title}</h1>
       {posts
-        .filter(post =>
-          post.frontMatter.tag.includes(decodeURIComponent(params.tag))
+        .filter((post) =>
+          post.frontMatter.tag.includes(decodeURIComponent(params.tag)),
         )
-        .map(post => (
+        .map((post) => (
           <PostCard key={post.route} post={post} />
         ))}
     </>
-  )
+  );
 }
--- eslint.config.js
+++ eslint.config.js
@@ -1,26 +1,35 @@
-import prettier from "eslint-plugin-prettier";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";;
+import prettier from 'eslint-plugin-prettier';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
+import unusedImports from 'eslint-plugin-unused-imports';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-export default [...compat.extends("next/core-web-vitals", "prettier"), {
+export default [
+  ...compat.extends('next/core-web-vitals', 'prettier'),
+  {
     plugins: {
-        prettier,
+      'unused-imports': unusedImports,
+      prettier,
     },
     rules: {
-        "prettier/prettier": "error",
+      'prettier/prettier': 'error',
 
-        "import/no-anonymous-default-export": ["error", {
-            allowObject: true,
-        }],
+      'import/no-anonymous-default-export': [
+        'none',
+        {
+          allowObject: true,
+        },
+      ],
+      'unused-imports/no-unused-imports': 'error',
     },
-}];
+  },
+];
